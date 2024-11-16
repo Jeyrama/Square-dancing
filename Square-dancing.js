6f22b77b068e50009984c93c8a320afe1850d559
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-15 20:34:01 -0800

    Update Square-dancing.js

--- Square-dancing.js
+++ Square-dancing.js
@@ -28,3 +28,12 @@ function differenceOfSquares(n){
 }
 
 // or
+
+function differenceOfSquares(x){
+  let sum = 0, squareSum = 0, i;
+  for (i =1 ; i <= x; i++) {
+    sum += i*i;
+    squareSum += i;
+  }
+  return squareSum*squareSum - sum;
+}
